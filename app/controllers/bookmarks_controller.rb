356d7e977ded03b1d38b1db8f747581046266415
linglee <amber36588888@gmail.com>
2025-02-14 12:42:46 +0100

    remove movies view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,70 +1,48 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: %i[ show edit update destroy ]
+  before_action :set_list, only: [:new, :create]
+  before_action :set_bookmark, only: [:destroy]
 
-  # GET /bookmarks or /bookmarks.json
   def index
     @bookmarks = Bookmark.all
   end
 
-  # GET /bookmarks/1 or /bookmarks/1.json
-  def show
-  end
-
-  # GET /bookmarks/new
   def new
     @bookmark = Bookmark.new
   end
 
-  # GET /bookmarks/1/edit
-  def edit
-  end
-
-  # POST /bookmarks or /bookmarks.json
   def create
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    # @bookmark.movie = movie
 
-    respond_to do |format|
-      if @bookmark.save
-        format.html { redirect_to @bookmark, notice: "Bookmark was successfully created." }
-        format.json { render :show, status: :created, location: @bookmark }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @bookmark.errors, status: :unprocessable_entity }
-      end
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /bookmarks/1 or /bookmarks/1.json
-  def update
-    respond_to do |format|
-      if @bookmark.update(bookmark_params)
-        format.html { redirect_to @bookmark, notice: "Bookmark was successfully updated." }
-        format.json { render :show, status: :ok, location: @bookmark }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @bookmark.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /bookmarks/1 or /bookmarks/1.json
   def destroy
-    @bookmark.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to bookmarks_path, status: :see_other, notice: "Bookmark was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
+
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bookmark
-      @bookmark = Bookmark.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def bookmark_params
-      params.require(:bookmark).permit(:comment, :movie_id, :list_id)
-    end
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  # def set_movie
+  #   @movie = Movie.find(params[:id])
+  # end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
 end
